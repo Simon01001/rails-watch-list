3e36f8d186d232ce1163647f62a65bc19cad037e
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-26 16:57:26 +0100

    add some changes to list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,14 +2,15 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
-  end
-
-  def new
     @list = List.new
   end
 
   def show
     @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
   end
 
   def create
